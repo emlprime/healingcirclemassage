dab2ae31f749f6674d54a683c1482d336fa09ad1
Peter Stradinger <peter@cutopia.(none)>
2008-07-19 09:35:31 -0700

    adding in initial import

---
+++ urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import *
+from healingcirclenaturalhealth.settings import MEDIA_ROOT
+
+urlpatterns = patterns('django.views.generic.simple',
+    (r'^$', 'direct_to_template', {'template': 'home.html'}),
+    (r'^services/$', 'direct_to_template', {'template': 'services.html'}),
+    (r'^consultation/$', 'direct_to_template', {'template': 'consultation.html'}),
+    (r'^resume/$', 'direct_to_template', {'template': 'resume.html'}),
+    (r'^intake/$', 'direct_to_template', {'template': 'intake.html'}),
+    (r'^help/$', 'direct_to_template', {'template': 'help.html'}),
+)
+
+urlpatterns += patterns('',
+    (r'^media/(.*)$', 'django.views.static.serve', {'document_root': MEDIA_ROOT}),
+)
